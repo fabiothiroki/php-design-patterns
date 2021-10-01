0de20950603cf25e6186579f85ba6940413e9f9c
Fabio Hiroki <fabio.hiroki@mollie.com>
2021-10-01 19:38:25 +0200

    add simple test

---
+++ tests/Test.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+final class Test extends TestCase
+{
+    public function testOutputMessage()
+    {
+        require_once '../app/index.php';
+
+        $this->expectOutputString('Hello world!');
+    }
+}
